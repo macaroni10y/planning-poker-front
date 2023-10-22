d9fae99d697ebccd9666d57489f24c86d7dce9f1
kitajima.yuto <kitajima.yuto@furyu.jp>
2023-10-22 23:57:05 +0900

    set root page as login

--- middleware/loginCheck.js
+++ middleware/loginCheck.js
@@ -3,6 +3,6 @@ import Cookies from 'js-cookie';
 
 export default ({redirect, route}) => {
   const userInfo = Cookies.get('userInfo');
-  if(route.path === '/login' && userInfo) return redirect('/top');
-  if (route.path !== '/login' && !userInfo) return redirect('/login');
+  if(route.path === '/' && userInfo) return redirect('/top');
+  if (route.path !== '/' && !userInfo) return redirect('/');
 }
